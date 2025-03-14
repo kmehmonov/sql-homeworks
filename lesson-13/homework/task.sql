92b0c5646dcb0d3d3df23b55fa032bb1fafefcc5
kamoliddinMehmonov <kamoliddinmehmonov4ionplazma@gmail.com>
2025-03-14 16:37:03 +0500

    lesson-13 homeworrk done

--- lesson-13/homework/task.sql
+++ lesson-13/homework/task.sql
@@ -1,10 +1,11 @@
 USE lesson13;
 GO
 
-declare @InputDate DATE = '2021-09-01';
+--Enter a date to see that month calendar
+declare @InputDate DATE = '2094-12-21';
 
 ;WITH cte AS
-(
+	(
     -- Start from the 1st of the month
     SELECT 
         DATEFROMPARTS(YEAR(@InputDate), MONTH(@InputDate), 1) AS [Date], 
@@ -20,18 +21,27 @@ declare @InputDate DATE = '2021-09-01';
     WHERE [Date] < EOMONTH(@InputDate)
 ),
 cte2 AS
-(
+	(
     -- Add a week number to group weeks
     SELECT 
         [Date], 
         DATENAME(WEEKDAY, [Date]) AS [DayName],
-        DATEPART(WEEKDAY, [Date]) AS WeekDayNumber,
-        DENSE_RANK() OVER (ORDER BY DATEADD(DAY, -DATEPART(WEEKDAY, [Date]), [Date])) AS WeekNum
+        WeekDayNumber,
+		--DATEADD(DAY, -WeekDayNumber, [Date]) as new,
+        DENSE_RANK() OVER (ORDER BY DATEADD(DAY, -WeekDayNumber, [Date])) AS WeekNum
     FROM cte
 )
 
-
-select * from cte2;
-
-select DATEPART(WEEKDAY, '2024-10-25')
-
+-- convert into calendar table
+SELECT 
+    WeekNum,
+    MAX(CASE WHEN DayName = 'Sunday' THEN DAY([Date]) END) AS Sunday,
+    MAX(CASE WHEN DayName = 'Monday' THEN DAY([Date]) END) AS Monday,
+    MAX(CASE WHEN DayName = 'Tuesday' THEN DAY([Date]) END) AS Tuesday,
+    MAX(CASE WHEN DayName = 'Wednesday' THEN DAY([Date]) END) AS Wednesday,
+    MAX(CASE WHEN DayName = 'Thursday' THEN DAY([Date]) END) AS Thursday,
+    MAX(CASE WHEN DayName = 'Friday' THEN DAY([Date]) END) AS Friday,
+    MAX(CASE WHEN DayName = 'Saturday' THEN DAY([Date]) END) AS Saturday
+FROM cte2
+GROUP BY WeekNum
+ORDER BY WeekNum;
